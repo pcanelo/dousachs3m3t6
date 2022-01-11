7b56f5f5c222cd1a2a661ca92b2d7c6e61ba10cd
Patricio Canelo <pcanelo@gmail.com>
2022-01-11 18:44:44 -0300

    commit 1 para subit

---
+++ src/main/java/com/dousachm3s3t6/example/RestApiApplication.java
@@ -0,0 +1,26 @@
+package com.dousachm3s3t6.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.dousachm3s3t6.example.model.Student;
+import com.dousachm3s3t6.example.repository.StudentRepository;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(StudentRepository repository) {
+		return args -> {
+			repository.save(new Student("Juan", "Perez", "Junior"));
+			repository.save(new Student("Marcos", "Perez", "Senior"));
+			repository.save(new Student("Macario", "Olivares", "Goma"));
+		};
+	}
+}
